002405cd6c46f5180a866a80d0ecfcb8f6e9dbda
Haley Guerra <hltroyer@gmail.com>
2026-07-05 08:05:52 -0400

    Initial Firebase project setup for ToDoList

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAroeUW_vZhe51kz38L7FJWaAtBcKV8ppE",
+  authDomain: "todolist-8dcca.firebaseapp.com",
+  projectId: "todolist-8dcca",
+  storageBucket: "todolist-8dcca.firebasestorage.app",
+  messagingSenderId: "241842694317",
+  appId: "1:241842694317:web:4d73128ee38c94caeffe24"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
